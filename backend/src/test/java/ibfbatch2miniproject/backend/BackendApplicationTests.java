a4c21636a085ddbfaaa7b52c090385387dfbb08c
Shiyi <yeoshiyi22@gmail.com>
2023-05-22 18:08:07 +0800

    homepage

---
+++ backend/src/test/java/ibfbatch2miniproject/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ibfbatch2miniproject.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
